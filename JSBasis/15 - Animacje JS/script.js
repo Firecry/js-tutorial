9b2f5cabe7edc115355990c3b0195ab5d8031f12
Firecry <lubiexd80@gmail.com>
2023-11-09 17:11:25 +0100

    lesson 15th

---
+++ JSBasis/15 - Animacje JS/script.js
@@ -0,0 +1,46 @@
+// Animacje
+
+let box1 = document.querySelector('#box1');
+let box2 = document.querySelector('#box2');
+
+// setInterval(()=>{
+//     console.log(box1.getBoundingClientRect().top);
+// }, 1000);
+
+box1.firstElementChild.classList.remove('inner');
+box1.firstElementChild.classList.add('inner2');
+
+box2.firstElementChild.classList.remove('inner');
+box2.firstElementChild.classList.add('inner2');
+
+let interval = setInterval(()=>{
+    box1.getBoundingClientRect().top <= 0 ? changeClass() : null;
+}, 1000/60);
+
+function changeClass(){
+    box1.firstElementChild.classList.remove('inner2');
+    box1.firstElementChild.classList.add('inner');
+    clearInterval(interval);
+}
+
+let interval2 = setInterval(()=>{
+    box2.getBoundingClientRect().top <= 0 ? startAnimation() : null;
+}, 1000/60);
+
+let interval3;
+
+function startAnimation(){
+    interval3 = setInterval(animation, 1000/60);
+    clearInterval(interval2);
+}
+
+let pos = -50;
+
+function animation(){
+    pos += 1;
+    box2.firstElementChild.style.left = `${pos}%`
+    if(pos >= 50){
+        clearInterval(interval3);
+    };
+    // pos >= 50 ? clearInterval(interval3) : null;
+}
